198db7a6a8e5fb12ceb9802895ca42c96ad9b743
Wembo Mulumba <fabriceotepa@hotmail.com>
2017-12-06 20:42:12 -0600

    Update Serve.js

--- Serve.js
+++ Serve.js
@@ -1,33 +1,58 @@
+// MEAN Stack RESTful API Tutorial - Contact List App
+
 var express = require('express');
 var app = express();
+var mongojs = require('mongojs');
+var db = mongojs('contactlist', ['contactlist']);
+var bodyParser = require('body-parser');
 
-app.use(express.static(__dirname + "/public"));
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.json());
 
-app.get('/contactlist', function(req,res){
-    
-    console.log('I received a GET request');
-    
-               person1 = {            
-                    name: 'Tim',
-                    email : 'tim2@yahoo.fr',
-                    number : '111-433-847'
-                
-                };              
-                person2 = {               
-                    name: 'EmiluTim',
-                    email : 'Emilytim2@yahoo.fr',
-                    number : '131-433-847'
-                
-                };  
-                person3 = {               
-                    name: 'reEmiluTim',
-                    email : 'erEmilytim2@yahoo.fr',
-                    number : '431-433-847'              
-                };
-    
-    var contactlist = [person1, person2, person3];
-    res.json(contactlist);
+app.get('/contactlist', function (req, res) {
+  console.log('I received a GET request');
+
+  db.contactlist.find(function (err, docs) {
+    console.log(docs);
+    res.json(docs);
+  });
 });
 
-app.listen(5454);
+app.post('/contactlist', function (req, res) {
+  console.log(req.body);
+  db.contactlist.insert(req.body, function(err, doc) {
+    res.json(doc);
+  });
+});
+
+app.delete('/contactlist/:id', function (req, res) {
+  var id = req.params.id;
+  console.log(id);
+  db.contactlist.remove({_id: mongojs.ObjectId(id)}, function (err, doc) {
+    res.json(doc);
+  });
+});
+
+app.get('/contactlist/:id', function (req, res) {
+  var id = req.params.id;
+  console.log(id);
+  db.contactlist.findOne({_id: mongojs.ObjectId(id)}, function (err, doc) {
+    res.json(doc);
+  });
+});
+
+app.put('/contactlist/:id', function (req, res) {
+  var id = req.params.id;
+  console.log(req.body.name);
+  db.contactlist.findAndModify({
+    query: {_id: mongojs.ObjectId(id)},
+    update: {$set: {name: req.body.name, email: req.body.email, number: req.body.number}},
+    new: true}, function (err, doc) {
+      res.json(doc);
+    }
+  );
+});
+
+app.listen(3000);
+console.log("Server running on port 3000");
 console.log('Serve is running on localhost port 3000');
